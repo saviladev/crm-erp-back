d557ca2c49311bd8d820074368f8ec0f2a57637d
Rodrigo Guevara <roalgenio14@gmail.com>
2025-07-12 16:08:14 -0500

    fix install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ php artisan jwt:secret
 sudo chmod -R 775 /var/www/web-back /var/www/
 sudo chown -R www-data:www-data /var/www/web-back
 
-sudo cp /install/dir.conf /etc/apache2/mods-enabled/
-sudo cp /install/laravel.conf /etc/apache2/sites-available/
+sudo cp install/dir.conf /etc/apache2/mods-enabled/
+sudo cp install/laravel.conf /etc/apache2/sites-available/
 
 sudo a2ensite laravel
 sudo a2enmod rewrite
